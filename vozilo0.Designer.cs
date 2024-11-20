2c5f3b15e4cba2e8fb23146c6a20eea2cca6165b
Benko3 <152974533+Benko3@users.noreply.github.com>
2024-11-20 09:29:19 +0100

    Add files via upload

---
+++ vozilo0.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace Vozilooo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMarka = new System.Windows.Forms.Label();
+            this.txtModel = new System.Windows.Forms.Label();
+            this.txtGodinaProizvodnje = new System.Windows.Forms.Label();
+            this.txtKilometraza = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.txtDodajVozilo = new System.Windows.Forms.Button();
+            this.txtSortitaj = new System.Windows.Forms.Button();
+            this.listBoxVozila = new System.Windows.Forms.ListBox();
+            this.comboBoxSortiranje = new System.Windows.Forms.ComboBox();
+            this.txtObrisiVozilo = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtMarka
+            // 
+            this.txtMarka.AutoSize = true;
+            this.txtMarka.Location = new System.Drawing.Point(34, 35);
+            this.txtMarka.Name = "txtMarka";
+            this.txtMarka.Size = new System.Drawing.Size(53, 20);
+            this.txtMarka.TabIndex = 0;
+            this.txtMarka.Text = "Marka";
+            // 
+            // txtModel
+            // 
+            this.txtModel.AutoSize = true;
+            this.txtModel.Location = new System.Drawing.Point(34, 74);
+            this.txtModel.Name = "txtModel";
+            this.txtModel.Size = new System.Drawing.Size(52, 20);
+            this.txtModel.TabIndex = 1;
+            this.txtModel.Text = "Model";
+            // 
+            // txtGodinaProizvodnje
+            // 
+            this.txtGodinaProizvodnje.AutoSize = true;
+            this.txtGodinaProizvodnje.Location = new System.Drawing.Point(34, 118);
+            this.txtGodinaProizvodnje.Name = "txtGodinaProizvodnje";
+            this.txtGodinaProizvodnje.Size = new System.Drawing.Size(142, 20);
+            this.txtGodinaProizvodnje.TabIndex = 2;
+            this.txtGodinaProizvodnje.Text = "GodinaProizvodnje";
+            // 
+            // txtKilometraza
+            // 
+            this.txtKilometraza.AutoSize = true;
+            this.txtKilometraza.Location = new System.Drawing.Point(34, 168);
+            this.txtKilometraza.Name = "txtKilometraza";
+            this.txtKilometraza.Size = new System.Drawing.Size(92, 20);
+            this.txtKilometraza.TabIndex = 3;
+            this.txtKilometraza.Text = "Kilometraza";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(182, 29);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(182, 74);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 26);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(182, 118);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 26);
+            this.textBox3.TabIndex = 6;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(182, 162);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 26);
+            this.textBox4.TabIndex = 7;
+            // 
+            // txtDodajVozilo
+            // 
+            this.txtDodajVozilo.Location = new System.Drawing.Point(27, 224);
+            this.txtDodajVozilo.Name = "txtDodajVozilo";
+            this.txtDodajVozilo.Size = new System.Drawing.Size(111, 31);
+            this.txtDodajVozilo.TabIndex = 8;
+            this.txtDodajVozilo.Text = "DodajVozilo";
+            this.txtDodajVozilo.UseVisualStyleBackColor = true;
+            this.txtDodajVozilo.Click += new System.EventHandler(this.txtDodajVozilo_Click);
+            // 
+            // txtSortitaj
+            // 
+            this.txtSortitaj.Location = new System.Drawing.Point(438, 103);
+            this.txtSortitaj.Name = "txtSortitaj";
+            this.txtSortitaj.Size = new System.Drawing.Size(75, 23);
+            this.txtSortitaj.TabIndex = 9;
+            this.txtSortitaj.Text = "Sortiraj";
+            this.txtSortitaj.UseVisualStyleBackColor = true;
+            this.txtSortitaj.Click += new System.EventHandler(this.txtSortitaj_Click);
+            // 
+            // listBoxVozila
+            // 
+            this.listBoxVozila.FormattingEnabled = true;
+            this.listBoxVozila.ItemHeight = 20;
+            this.listBoxVozila.Location = new System.Drawing.Point(27, 298);
+            this.listBoxVozila.Name = "listBoxVozila";
+            this.listBoxVozila.Size = new System.Drawing.Size(120, 84);
+            this.listBoxVozila.TabIndex = 10;
+            this.listBoxVozila.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // comboBoxSortiranje
+            // 
+            this.comboBoxSortiranje.FormattingEnabled = true;
+            this.comboBoxSortiranje.Location = new System.Drawing.Point(452, 218);
+            this.comboBoxSortiranje.Name = "comboBoxSortiranje";
+            this.comboBoxSortiranje.Size = new System.Drawing.Size(121, 28);
+            this.comboBoxSortiranje.TabIndex = 11;
+            // 
+            // txtObrisiVozilo
+            // 
+            this.txtObrisiVozilo.Location = new System.Drawing.Point(182, 224);
+            this.txtObrisiVozilo.Name = "txtObrisiVozilo";
+            this.txtObrisiVozilo.Size = new System.Drawing.Size(109, 31);
+            this.txtObrisiVozilo.TabIndex = 12;
+            this.txtObrisiVozilo.Text = "ObrisiVozilo";
+            this.txtObrisiVozilo.UseVisualStyleBackColor = true;
+            this.txtObrisiVozilo.Click += new System.EventHandler(this.txtObrisiVozilo_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtObrisiVozilo);
+            this.Controls.Add(this.comboBoxSortiranje);
+            this.Controls.Add(this.listBoxVozila);
+            this.Controls.Add(this.txtSortitaj);
+            this.Controls.Add(this.txtDodajVozilo);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtKilometraza);
+            this.Controls.Add(this.txtGodinaProizvodnje);
+            this.Controls.Add(this.txtModel);
+            this.Controls.Add(this.txtMarka);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label txtMarka;
+        private System.Windows.Forms.Label txtModel;
+        private System.Windows.Forms.Label txtGodinaProizvodnje;
+        private System.Windows.Forms.Label txtKilometraza;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button txtDodajVozilo;
+        private System.Windows.Forms.Button txtSortitaj;
+        private System.Windows.Forms.ListBox listBoxVozila;
+        private System.Windows.Forms.ComboBox comboBoxSortiranje;
+        private System.Windows.Forms.Button txtObrisiVozilo;
+    }
+}
+
---
+++ vozilo0.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vozilooo
+{
+    public partial class Form1 : Form
+    {
+        private List<Vozilo> vozila = new List<Vozilo>();
+        public Form1()
+        {
+            InitializeComponent();
+
+
+            comboBoxSortiranje.Items.Add("Marka");
+            comboBoxSortiranje.Items.Add("Model");
+            comboBoxSortiranje.Items.Add("GodinaProizvodnje");
+            comboBoxSortiranje.Items.Add("Kilometraza");
+            comboBoxSortiranje.SelectedIndex = 0;
+        }
+
+        private void txtDodajVozilo_Click(object sender, EventArgs e)
+        {
+            string marka = txtMarka.Text;
+            string model = txtModel.Text;
+            int godinaProizvodnje;
+            int kilometraza;
+
+            if (int.TryParse(txtGodinaProizvodnje.Text, out godinaProizvodnje) && godinaProizvodnje > 0 &&
+            int.TryParse(txtKilometraza.Text, out kilometraza) && kilometraza >= 0)
+            {
+
+                Vozilo novoVozilo = new Vozilo(marka, model, godinaProizvodnje, kilometraza);
+                vozila.Add(novoVozilo);
+                MessageBox.Show("Vozilo dodano!", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Unesite ispravne podatke za godinu proizvodnje i kilometražu.", "Greška", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+
+        }
+
+        private void txtSortitaj_Click(object sender, EventArgs e)
+        {
+            var kriterij = comboBoxSortiranje.SelectedItem.ToString();
+            IEnumerable<Vozilo> sortiranaLista;
+
+
+            switch (kriterij)
+            {
+                case "Marka":
+                    sortiranaLista = vozila.OrderBy(v => v.Marka);
+                    break;
+                case "Model":
+                    sortiranaLista = vozila.OrderBy(v => v.Model);
+                    break;
+                case "GodinaProizvodnje":
+                    sortiranaLista = vozila.OrderBy(v => v.GodinaProizvodnje);
+                    break;
+                case "Kilometraza":
+                    sortiranaLista = vozila.OrderBy(v => v.Kilometraza);
+                    break;
+                default:
+                    sortiranaLista = vozila;
+                    break;
+            }
+            
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtObrisiVozilo_Click(object sender, EventArgs e)
+        {
+            if (listBoxVozila.SelectedItem != null)
+            {
+                Vozilo voziloZaBrisanje = (Vozilo)listBoxVozila.SelectedItem;
+                vozila.Remove(voziloZaBrisanje);
+                
+                MessageBox.Show("Vozilo je obrisano.", "Info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Odaberite vozilo koje želite obrisati.", "Greška", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
